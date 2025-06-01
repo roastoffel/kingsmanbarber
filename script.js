4cdc6cb1bb47a5db898407f04f7f980f9fc56998
Rodrigo Stoffel <rodrigo.stoffel@gmail.com>
2025-06-02 00:09:02 +0100

    New section 'BARBEIROS' created

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuToggle = document.querySelector(".menu-toggle");
   const navUl = document.querySelector("nav ul");
+  const header = document.querySelector("header");
+  const body = document.body;
+
+  function adjustBodyPadding() {
+    const headerHeight = header.offsetHeight;
+    body.style.paddingTop = `${headerHeight}px`;
+  }
+
+  adjustBodyPadding();
+
+  window.addEventListener("resize", adjustBodyPadding);
 
   menuToggle.addEventListener("click", function () {
     navUl.classList.toggle("show");
